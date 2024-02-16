e81687a94ae12a682a59f0040d8c04e52b9d6f62
Sumit <sumitmitra98@gmail.com>
2024-02-16 12:34:57 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,32 @@ app.get('/', (req,res)=> {
     res.send('Server is ready');
 });
 
+app.get('/jokes', (req,res)=> {
+    const jokes = [
+        {
+            id:1,
+            name:'joke-1',
+        },
+        {
+            id:2,
+            name:'joke-2',
+        },
+        {
+            id:3,
+            name:'joke-3',
+        },
+        {
+            id:4,
+            name:'joke-4',
+        },
+        {
+            id:5,
+            name:'joke-5',
+        }
+    ];
+    res.send(jokes);
+})
+
 const port = process.env.PORT || 4000 ;
 
 app.listen(port, ()=> {
